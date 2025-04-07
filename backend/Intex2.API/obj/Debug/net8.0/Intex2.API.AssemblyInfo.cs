6548dd59200632701a411b09ccedd865264bb941
Logan Spencer <loganspencer203@gmail.com>
2025-04-07 13:31:54 -0600

    Controllers

--- backend/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
+++ backend/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3a9adf2845e52b95d7fb2dc345d630c21b627e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
