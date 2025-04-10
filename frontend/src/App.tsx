c63d016f09de5d56f8b7c7c58dcd7198ea58bf97
carson46 <carson46@byu.edu>
2025-04-10 11:44:46 -0600

    checkpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-<<<<<<< HEAD
-=======
 
->>>>>>> 30db00247032a0c483a8a3ca566a8305963b9b27
 import LandingPage from "./pages/LandingPage";
 import CreateProfile from "./pages/CreateProfile";
 import MovieDisplay from "./pages/MovieDisplay";
@@ -16,13 +13,11 @@ import AddMoviePage from "./pages/AddMoviePage";
 import AdminMoviesPage from "./pages/AdminMoviePage";
 import CookieConsent from "./components/CookieConsent";
 import MovieModal from "./components/MovieModel";
-<<<<<<< HEAD
 import GenrePage from "./pages/GenrePage";
-=======
 import Unauthorized from "./pages/Unauthorized";
 import ProtectedRoute from "./components/ProtectedRoute";
->>>>>>> 30db00247032a0c483a8a3ca566a8305963b9b27
 import { Movie } from "./types/Movie";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
@@ -33,43 +28,6 @@ const App = () => {
         <Header onMovieSelect={(movie: Movie) => setSelectedMovie(movie)} />
 
         <Routes>
-<<<<<<< HEAD
-          {/* No header pages */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/create-profile" element={<CreateProfile />} />
-          <Route path="/login" element={<LoginPage />} />
-
-          {/* All routes that use Header */}
-          <Route
-            path="*"
-            element={
-              <>
-                <Header onMovieSelect={(movie: Movie) => setSelectedMovie(movie)} />
-                <Routes>
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/movies" element={<MovieDisplay />} />
-                  <Route path="/genres" element={<GenrePage />} />
-                  <Route path="/recommended" element={<RecommendedDisplay />} />
-                  <Route path="/privacy" element={<PrivacyPage />} />
-                  <Route path="/adminMovies" element={<AdminMoviesPage />} />
-                  <Route
-                    path="/addMovie"
-                    element={
-                      <AddMoviePage
-                        onSuccess={() => console.log("Movie added successfully")}
-                        onCancel={() => console.log("Movie addition canceled")}
-                      />
-                    }
-                  />
-                </Routes>
-              </>
-            }
-          />
-        </Routes>
-      </Router>
-
-      {/* Movie Modal shown globally if a movie is selected */}
-=======
           {/* Public routes without layout */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/create-profile" element={<CreateProfile />} />
@@ -129,7 +87,6 @@ const App = () => {
       </Router>
 
       {/* Movie modal if selected */}
->>>>>>> 30db00247032a0c483a8a3ca566a8305963b9b27
       {selectedMovie && (
         <MovieModal
           movieId={selectedMovie.showId ?? ""}
