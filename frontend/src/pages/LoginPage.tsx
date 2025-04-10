84dacfb84a8416beae104c56cc8d09e6c7d77781
Logan Spencer <loganspencer203@gmail.com>
2025-04-09 19:11:03 -0600

    Updating

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,14 +1,14 @@
-// src/pages/LoginPage.tsx
-import React from "react"; // Import React
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import React from "react"; 
+import { useNavigate } from "react-router-dom"; 
 import "../components/LoginPage.css"; 
-import LoginForm from "../components/LoginForm"; // adjust the path if needed
+import LoginForm from "../components/LoginForm"; 
+
 const LoginPage: React.FC = () => {
-  const navigate = useNavigate(); // Define navigate using useNavigate
+  const navigate = useNavigate(); 
+
   return (
-    
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <header className="header">
+    <div className="login-page">
+      <header >
         <div className="logo">CineNiche</div>
         <nav>
           <button className="sign-in-btn" onClick={() => navigate("/create-profile")}>
@@ -16,8 +16,9 @@ const LoginPage: React.FC = () => {
           </button>
         </nav>
       </header>
-      <div className="max-w-md w-full bg-white p-6 rounded-xl shadow-lg">
-        <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+
+      <div className="login-form-container">
+        <h1 className="login-title">Login</h1>
         <LoginForm />
       </div>
     </div>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      // 'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+    }
+  }
 })
